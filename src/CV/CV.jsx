ccb70889272e5ca48110a25177f83a3b245ae384
Luispa23 <luis.pablo@bootcamp-upgrade.com>
2022-01-21 19:31:59 +0100

    starting with personal data

---
+++ src/CV/CV.jsx
@@ -0,0 +1,83 @@
+export const CV = {
+    hero: {
+      name: "Luis Pablo",
+      adress: "Arroyo de la Encomienda",
+      city: "Valladolid",
+      email: "lp.delriom@gmail.com",
+      birthDate: "20/07/1985",
+      phone: "(+34) 685148169",
+      image: "",
+      gitLab: "https://gitlab.com/luispa23/",
+      aboutMe: [
+        {
+          info: "🤖 Failure doesn't mean the game is over, it means try again with experience.",
+        },
+        {
+          info: "🔩 FullStack Developer.",
+        },
+       
+      ],
+    },
+    education: [
+      {
+        name: "FullStack developer",
+        date: "2022",
+        where: "Upgrade - HUB",
+      },
+      {
+        name: "First Certificate in English",
+        date: "2014",
+        where: "Oxford University",
+      },
+      {
+        name: "Maestro de Educacion Primaria",
+        date: "2008-2012",
+        where: "Universidad de Valladolid",
+      },
+    ],
+    experience: [
+      {
+        name: "Adjunto REsponsable ALDI ",
+        date: "07/11/2017 – Nowadays",
+        where: "ALDI - Supermercados",
+        description:
+          "My job is team management, checking and maintaining stocks, placing orders, warehouse management, and merchandise presentation in the sales room..",
+      },
+      {
+        name: "Vendedor-Deportista (especialista Baloncesto)",
+        date: "15/10/20015 – 15/04/2016",
+        where: "Decathlon",
+        description:
+          "Attention to the public, stock management, presentation of merchandise, and placing orders to the warehouse",
+      },
+    ],
+    languages: {
+      language: "English",
+      wrlevel: "Native",
+      splevel: "Native",
+    },
+    habilities: [
+      "Robotics",
+      "Robot Programming",
+      "Physics",
+      "Weaponery",
+      "Engineer",
+      "Money",
+      "Dating",
+      "Saving the world",
+    ],
+    volunteer: [
+      {
+        name: "September Foundation",
+        where: "MIT",
+        description:
+          "The September Foundation is a program by Tony Stark to fund schools and young prodigies in their education. The foundation was named by Stark after a lyric from The Fantasticks song, 'Try to Remember,' which he heard his mother sing and play on the piano before her death.",
+      },
+      {
+        name: "Damage Control",
+        where: "U.S.A.",
+        description:
+          "The United States Department of Damage Control, occasionally known as the DODC, is a department of the United States of America. Initially a subsidiary of S.H.I.E.L.D., Damage Control was an organization specializing in post-battle clean-up. Following the Battle of New York, Damage Control was made into an executive branch of the United States government, and in a joint venture with Stark Industries, was tasked with acquiring alien and other dangerous artifacts along with cleaning up damages caused by enhanced individuals.",
+      },
+    ],
+  };
