f537bf4dde764d80bcd952a46e3b0243a24ef4f1
Luispa23 <luis.pablo@bootcamp-upgrade.com>
2022-02-07 09:59:27 +0100

    last changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import React, {useState} from 'react';
-import{ Router, Route, Routes, useNavigate } from 'react-router-dom';
+import{ Route, Routes, useNavigate } from 'react-router-dom';
 //import { useNavigate } from 'react-router-dom';
 
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,12 +10,15 @@ const Navbar = () => {
                <button>Home</button>
             </NavLink>
 
-             <NavLink to="/login">
+             {/* <NavLink to="/login">
                <button>Login</button>
+            </NavLink> */}
+
+
+             <NavLink to="/profile">
+               <button>Profile</button>
             </NavLink>
 
-
-         
 
             <NavLink to="/education">
                 <button>Education</button>
